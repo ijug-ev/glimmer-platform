d21e8f42ddc0420d7dc32495a14d321824a5e2d2
Tobias Frech <tobias@frech.info>
2022-12-16 10:35:21 +0100

    Prüfe, ob Server existiert

--- scripts/jitsi-snapshot.sh
+++ scripts/jitsi-snapshot.sh
@@ -1,1 +1,10 @@
-hcloud server create-image --description "Jitsi-Meet letzter Stand `date -I`" --type snapshot --label "version=current,app=jitsi" jitsi-meet 
+SERVERID=$(hcloud server list -o columns=id,name|grep jitsi-meet|cut -d" " -f1)
+
+if [ -z "$SERVERID" ]; then
+ echo Server jitsi-meet nicht gefunden, breche ab.
+ exit 1
+else
+ echo Server jitsi-meet hat die ID $SERVERID
+fi
+
+hcloud server create-image --description "Jitsi-Meet letzter Stand `date -I`" --type snapshot --label "version=current,app=jitsi" jitsi-meet
